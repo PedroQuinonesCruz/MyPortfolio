1777fce1050be83b25b348f4a266f794950d8ada
Pedro Quiñones Cruz <peter61970@hotmail.com>
2024-02-18 23:11:36 -0600

    Firts Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+import Navbar from './components/Navbar';
+import Hiro from './components/Hiro';
+import Skills from './components/Skills'
+import Honors from './components/Honors';
+import Certs from './components/Certs';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+    useEffect(() => {
+      document.title = 'Pedro Quiñones Cruz';
+      AOS.init();
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hola mundo! </h1>
-        <h1> Este sera mi portafolio </h1>
-        <h1> Hey soy yo de nuevo </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
+      <Navbar />
+      <Hiro />
+      <Skills />
+      <Honors />
+      <Certs />
+      <Contact />
+      <Footer />
     </div>
   );
 }
